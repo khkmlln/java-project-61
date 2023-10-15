19181feec060d5e0297ab40292647db06109a8f0
khkmlln <alsu.hakimullina@yandex.ru>
2023-10-15 20:27:12 +0300

    trying to modify my code and make it more cool

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,13 +1,6 @@
 package hexlet.code;
 import java.util.Scanner;
 public class App {
-    private static final int GREETING = 1;
-    private static final int EVEN = 2;
-    private static final int CALCULATOR = 3;
-    private static final int GCD = 4;
-    private static final int PROGRESSION = 5;
-    private static final int PRIME = 6;
-    private static final int EXIT = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter");
@@ -20,43 +13,51 @@ public class App {
         System.out.println("0 - Exit");
         System.out.println("Your choice: ");
         int choice = scanner.nextInt();
-        if (choice == GREETING) {
+        String name;
+        switch (choice) {
+            case '1':
             System.out.println("Your choice is 1");
             Cli.greetUser();
-        } else if (choice == EXIT) {
+            break;
+            case '0':
             System.out.println("Your choice is 0");
             System.out.println("Bye");
-        } else if (choice == EVEN) {
+            break;
+            case '2':
             System.out.println("Your choice is 2");
             System.out.println("Welcome to the Brain Games!");
             System.out.println("May i have your name?");
-            String name = scanner.next();
+            name = scanner.next();
             Engine.playEvenGame(scanner, name);
-        } else if (choice == CALCULATOR) {
+            break;
+            case '3':
             System.out.println("Your choice is 3");
             System.out.println("Welcome to the Brain Games!");
             System.out.println("May i have your name?");
-            String name = scanner.next();
+            name = scanner.next();
             Engine.calculatorGame(scanner, name);
-        } else if (choice == GCD) {
+            case '4':
             System.out.println("Your choice is 4");
             System.out.println("Welcome to the Brain games!");
             System.out.println("May i have your name?");
-            String name = scanner.next();
+            name = scanner.next();
             Engine.gcdGame(scanner, name);
-        } else if (choice == PROGRESSION) {
+            break;
+            case '5':
             System.out.println("Your choice is 5");
             System.out.println("Welcome to the Brain games!");
             System.out.println("May i have your name?");
-            String name = scanner.next();
+            name = scanner.next();
             Engine.progressionGame(scanner, name);
-        } else if (choice == PRIME) {
+            break;
+            case '6':
             System.out.println("Your choice is 6");
             System.out.println("Welcome to the Brain games!");
             System.out.println("May i have your name?");
-            String name = scanner.next();
+            name = scanner.next();
             Engine.primeGame(scanner, name);
-        } else {
+            break;
+            default:
             System.out.println("Please, make sure that you chose the right number");
         }
     }
---
+++ app/src/main/java/hexlet/code/EvenGame.java
@@ -0,0 +1,52 @@
+package hexlet.code;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Even {
+    private static boolean isEven(int number) {
+       return number % 2 == 0; // тут проверяем что четное или нечетное и возвращаем boolean
+    }
+
+    private static String[] generateRoundData() {
+        Random random = new Random();
+        int number = random.nextInt(100);// генерируем случайное число;
+        String question = String.valueOf(number);// конвертируем в строку;
+        String answer = isEven(number) ? "yes" : "no";// вычисляем правильный ответ для числа - "yes"|"no";
+        return new String[] {question, answer}; // тут метод возвращает одномерный массив [5, "no"]
+    }
+
+    public static void runGame() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        String name = scanner.next();
+        System.out.println("Hello, " + name + "!");
+        final var description = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+
+        String[][] roundsData = new String[3][2]; // создаем пустой массив размера число 3 на 2 [ [], [], [] ]
+
+        for (int i = 0; i < 3; i += 1) {
+            roundsData[i] = generateRoundData(); // записываем в массив 3 на 2 результат генерации раунда
+            // i = 0; [ [5, "no"], [], [] ]
+            // i = 1; [ [5, "no"], [8, "yes"], [] ]
+            // i = 2; [ [5, "no"], [8, "yes"], [50, "yes"] ]
+        }
+
+        Engine.run(description, roundsData);
+        for (int i = 0; i < 3; i++) {
+            String question = roundsData[i][0];
+            String correctAnswer = roundsData[0][i];
+            System.out.println("Question: " + question);
+            System.out.println("Answer: ");
+            String userAnswer = scanner.next();
+            if (!userAnswer.equalsIgnoreCase(correctAnswer)) {
+                String part1 = ("'" + userAnswer + "'" + "is wrong answer ;(. Correct answer was");
+                System.out.println(part1 + "'" + correctAnswer + "'");
+                System.out.println("Let's try again, " + name + "!");
+                break;
+            }
+            System.out.println("Correct!");
+        }
+        System.out.println("Congratulations, " + name + ",");
+    }
+}
