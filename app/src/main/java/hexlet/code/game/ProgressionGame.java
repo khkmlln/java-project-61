4d2a97c8eeb8f62bfb1f9ac2233e70f5678b35b2
khkmlln <alsu.hakimullina@yandex.ru>
2023-11-02 01:05:45 +0300

    progression almost works

--- app/src/main/java/hexlet/code/game/ProgressionGame.java
+++ app/src/main/java/hexlet/code/game/ProgressionGame.java
@@ -3,7 +3,7 @@ import hexlet.code.Utils;
 import hexlet.code.Engine;
 public class ProgressionGame {
     private static final int BOUND = 100;
-    private static final int PROGRESSIONLENGTH = 5;
+    private static final int PROGRESSIONLENGTH = 8;
     private static final int RIGHTNUMBER = 3;
     private static final int ARRAYCOLUMNS = 3;
     private static final int ARRAYLINE = 2;
@@ -11,24 +11,30 @@ public class ProgressionGame {
 
 
     private static String[] generateRoundData() {
-        var start  = Utils.generateNumber(FIRSTBOUND, BOUND);
+        var start = Utils.generateNumber(FIRSTBOUND, BOUND);
         var step = Utils.generateNumber(FIRSTBOUND, BOUND);
         var hidden = Utils.generateNumber(0, PROGRESSIONLENGTH - 1);
-        String[] progression = makeProgression(start, step, hidden);
+        String[] progression = makeProgression(start, step, PROGRESSIONLENGTH);
         var answer = progression[hidden];
         progression[hidden] = "..";
         var question = String.join(" ", progression);
-        return new String[] {question, answer};
+        return new String[]{question, answer};
     }
-        private static String generateQuestion(String[] progression) {
+
+    private static String generateQuestion(String[] progression) {
         return String.join(" ", progression);
+    }
+
+    private static String generateAnswer(String[] progression, int hiddenIndex) {
+        if (hiddenIndex < 0) {
+            return "";
         }
-        private static String generateAnswer(String[] progression, int hiddenIndex) {
-            return progression[hiddenIndex];
-        }
+        return progression[hiddenIndex];
+    }
+
     public static void runGame() {
         final var description = "What number is missing in the progression?";
-                String[][] roundsData = new String[ARRAYCOLUMNS][ARRAYLINE];
+        String[][] roundsData = new String[ARRAYCOLUMNS][ARRAYLINE];
         for (int i = 0; i < RIGHTNUMBER; i++) {
             String[] progression = generateRoundData();
             int hiddenIndex = findHiddenIndex(progression);
@@ -38,6 +44,7 @@ public class ProgressionGame {
         }
         Engine.run(description, roundsData);
     }
+
     public static int findHiddenIndex(String[] progression) {
         for (int i = 0; i < progression.length; i++) {
             if (progression[i].equals("..")) {
@@ -46,12 +53,13 @@ public class ProgressionGame {
         }
         return -1;
     }
+
     private static String[] makeProgression(int first, int step, int length) {
-String[] progression = new String[length];
-for (int i = 0; i < length; i++) {
-    progression[i] = Integer.toString(first + i * step);
-}
-return progression;
+        String[] progression = new String[length];
+        for (int i = 0; i < length; i++) {
+            progression[i] = Integer.toString(first + i * step);
+        }
+        return progression;
     }
 }
 
