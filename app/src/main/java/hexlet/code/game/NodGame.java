a4eb619f46d6adb2082a1d990735f97047c5fdeb
khkmlln <alsu.hakimullina@yandex.ru>
2023-11-04 01:59:49 +0300

    deleted method i didnt need in nodgame

--- app/src/main/java/hexlet/code/game/NodGame.java
+++ app/src/main/java/hexlet/code/game/NodGame.java
@@ -6,13 +6,6 @@ public class NodGame {
     private static final int RIGHTNUMBER = 3;
     private static final int ARRAYCOLUMNS = 3;
     private static final int ARRAYLINE = 2;
-    private static int gcdGame(int number1, int number2) {
-        if (number2 == 0) {
-            return number1;
-        }
-        return gcdGame(number2, number1 % number2);
-    }
-
     private static String[] generateRoundData() {
         int number1 = Utils.generateNumber(0, BOUND);
         int number2 = Utils.generateNumber(0, BOUND);
